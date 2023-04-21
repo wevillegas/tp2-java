a9a2306cfe4426d6907a63300531415cedd23a2d
Wenceslao Villegas <wenceslaovillegas14@gmail.com>
2023-04-21 20:48:17 -0300

    punto 1

--- src/main/java/programacion2/tp2/TP2.java
+++ src/main/java/programacion2/tp2/TP2.java
@@ -1,16 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package programacion2.tp2;
+import java.util.Scanner;
 
-/**
- *
- * @author Wenceslao Villegas
- */
 public class TP2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int ejercicio;
+        Scanner teclado = new Scanner(System.in);
+        
+        P1 ejercicio1 = new P1();
+        ejercicio1.Ejercicio1();
     }
 }
