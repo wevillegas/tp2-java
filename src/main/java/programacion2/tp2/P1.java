fd5fe735695fe177742d06ca7c5ccc6d9419a735
Wenceslao Villegas <wenceslaovillegas14@gmail.com>
2023-04-21 20:48:26 -0300

    punto 1

---
+++ src/main/java/programacion2/tp2/P1.java
@@ -0,0 +1,14 @@
+package programacion2.tp2;
+
+// Escribir un programa que presente por pantalla los números del 1 al 20.
+public class P1 {
+   private int i = 1;
+   private int n = 20;
+    
+    public void Ejercicio1(){
+        while (i <= n) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
