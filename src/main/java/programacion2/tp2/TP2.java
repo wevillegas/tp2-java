fc662888a236f55f516aae8b766aaa99f8c4e404
Wenceslao Villegas <wenceslaovillegas14@gmail.com>
2023-04-28 21:08:38 -0300

    punto 2 tp2

--- src/main/java/programacion2/tp2/TP2.java
+++ src/main/java/programacion2/tp2/TP2.java
@@ -7,7 +7,11 @@ public class TP2 {
         int ejercicio;
         Scanner teclado = new Scanner(System.in);
         
-        P1 ejercicio1 = new P1();
-        ejercicio1.Ejercicio1();
+        // P1 ejercicio1 = new P1();
+        // ejercicio1.Ejercicio1();
+        // P2 ejercicio2 = new P2();
+        // ejercicio2.Ejercicio2();
+        P4 ejercicio4 = new P4();
+        ejercicio4.Ejercicio4();
     }
 }
