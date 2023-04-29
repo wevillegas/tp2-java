c18e9ce410b53f87b3bbc59b6bc934471077935e
Wenceslao Villegas <wenceslaovillegas14@gmail.com>
2023-04-28 21:09:06 -0300

    punto 2 tp2

---
+++ src/main/java/programacion2/tp2/P3.java
@@ -0,0 +1,20 @@
+package programacion2.tp2;
+
+import java.util.Scanner;
+
+public class P3 {
+
+    private float n, m;
+    private int i;
+    Scanner teclado = new Scanner(System.in);
+
+    public void Ejercicio3() {
+        do {
+            System.out.println("¿Cuantos numeros desea ingresar?");
+            n = teclado.nextInt();
+        } while (n <= 0);
+        m = n;
+        private int[] provArr = new int[m];
+    }
+
+}
